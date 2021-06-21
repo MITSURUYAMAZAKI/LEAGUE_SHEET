aeccae40019aa3ef2ceb0b665edaf855492baaa3
山崎満 <zakiyama.com12.aaa@gmail.com>
2021-06-21 20:01:23 +0900

    マスにチーム名を入れる

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
 
-    $teams = ["岩藤","ジェントル","ヨゴーズ","チャオズ","ザクレイ","ガナーズ","クサカク"];
+    $teams = ["岩藤","ジェントル","ヨゴーズ","チャオズ","ガナーズ","クサカク"];
 
 
 ?>
@@ -10,21 +10,25 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>LEAGUE_SHEET</title>
   <link rel="stylesheet" href="./style.css">
 </head>
 <body>
-  <div class="row"> 
+  <div class="row">
+  <!-- ↓１行目作成 -->
       <div class="box">＊</div>
       <?php foreach($teams as $team):?>
       <div class="box"><?php echo $team;?></div>
       <?php endforeach ?>
   </div>
+  <!-- ２行目以降作成 -->
   <?php foreach($teams as $team):?>
   <div class="row">
+      <!-- １列目 -->
       <div class="box"><?= $team?></div>
+      <!-- ２列目以降 -->
       <?php for ($i=0 ; $i< count($teams); $i++): ?>
-      <div class="box"></div>
+        <div class="box"><?= $team."×".$teams[$i];?></div>
       <?php endfor ?>
   </div>
   <?php endforeach ?>
